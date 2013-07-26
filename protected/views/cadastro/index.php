5d9fa1eeb44e2349cefe8388c262c1a6800193d9
Rafael <rafael.life@gmail.com>
2013-07-26 17:45:20 -0300

    Alterações 2

--- protected/views/cadastro/index.php
+++ protected/views/cadastro/index.php
@@ -3,10 +3,14 @@
 
 ?>
 <link rel="stylesheet" href="<?php echo Yii::app()->baseUrl?>/css/teste.css">
-<div class="page-header">
-    <h1><a class="well-large" href="<?php echo Yii::app()->baseUrl;?>/index.php">novidia</a> <small class="text-info">Novidades do dia dos melhores estabelecimentos!</small></h1>
-</div>
-<div class="row-fluid" style="">
+
+<div id="wrap">
+    <div class="row-fluid">
+    <div class="page-header">
+        <h1><a class="well-large" href="<?php echo Yii::app()->baseUrl;?>/index.php">novidia</a> <small class="text-info">Novidades do dia dos melhores estabelecimentos!</small></h1>
+    </div>
+    </div>
+    <div class="row-fluid" style="">
         <div class="form-inline text-center">
             <?php $form=$this->beginWidget('CActiveForm', array(
             'id'=>'usuario-form',
@@ -14,34 +18,35 @@
             'clientOptions'=>array(
                 'validateOnSubmit'=>true,
             ),
-            )); ?>
+        )); ?>
 
             <?php echo $form->errorSummary($model); ?>
 
-                <?php echo CHtml::activeTextField($model,
-                                                    'nome',
-                                                    array('id' => 'nome',
-                                                        'placeholder' => 'Seu Nome',
-                                                        'required' => 'true'))?>
+            <?php echo CHtml::activeTextField($model,
+            'nome',
+            array('id' => 'nome',
+                'placeholder' => 'Seu Nome',
+                'required' => 'true'))?>
 
-                <?php echo CHtml::activeTextField($model,
-                                                  'email',
-                                                  array('id' => 'usuario',
-                                                        'placeholder'=>'Seu Email',
-                                                        'required'=>'true'))?>
-                <?php echo CHtml::activeTextField($model,
-                                                    'email',
-                                                    array('id' => 'usuario_repeat',
-                                                        'placeholder'=>'Repita Seu Email',
-                                                        'required'=>'true'))?>
+            <?php echo CHtml::activeTextField($model,
+            'email',
+            array('id' => 'usuario',
+                'placeholder'=>'Seu Email',
+                'required'=>'true'))?>
+            <?php echo CHtml::activeTextField($model,
+            'email',
+            array('id' => 'usuario_repeat',
+                'placeholder'=>'Repita Seu Email',
+                'required'=>'true'))?>
 
-                <?php echo CHtml::activePasswordField($model,
-                                                      'senha',
-                                                       array('id' => 'senha',
-                                                              'placeholder' => 'Nova Senha',
-                                                              'required'=>'true')) ?>
+            <?php echo CHtml::activePasswordField($model,
+            'senha',
+            array('id' => 'senha',
+                'placeholder' => 'Nova Senha',
+                'required'=>'true')) ?>
 
-                <?php echo CHtml::submitButton('Registrar', array('class'=>'btn btn-large')) ?>
+            <?php echo CHtml::submitButton('Registrar', array('class'=>'btn btn-large')) ?>
             <?php $this->endWidget(); ?>
         </div>
+    </div>
 </div>
